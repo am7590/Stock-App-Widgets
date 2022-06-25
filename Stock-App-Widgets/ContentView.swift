a01e649490bbd9168f779c29e1aeae49bb308f9e
Alek Michelson <am7590@rit.edu>
2022-06-25 11:41:34 -0400

    Initial Commit

---
+++ Stock-App-Widgets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock-App-Widgets
+//
+//  Created by Alek Michelson on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stock-App-Widgets/Stock_App_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_App_WidgetsApp.swift
+//  Stock-App-Widgets
+//
+//  Created by Alek Michelson on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Stock_App_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
